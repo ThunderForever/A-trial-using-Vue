2bfc4d5efb7b7d94fb1bc0effdb1623ed048407d
DESKTOP-DCL8KK7\王力帆 <1710129303@qq.com>
2017-11-17 14:53:27 +0800

    store功能的实现

--- vuex/store.js
+++ vuex/store.js
@@ -8,7 +8,34 @@ const state={
     activeNote:{}
 }
 
-const mutations={}
+const mutations={
+    ADD_NOTE(state){
+        const newNote={
+            text:'New Note',
+            favorite:false
+        }
+        state.notes.push(newNote)
+        state.activeNote=newNote
+    },
+
+
+    EDIT_NOTE(state,text){
+        state.activeNote.text=text
+    },
+
+    DELETE_NOTE(state){
+        state.notes.$remove(state.activeNote)
+        state.activeNote=state.notes(0)
+    },
+
+    TOGGLE_FAVORITE(state){
+        state.activeNote.favorite=!state.activeNote.favorite
+    },
+
+    SET_ACTIVE_NOTE(state,note){
+        state.activeNote=newNote
+    }
+}
 
 export default new Vuex.Store({
     state,
